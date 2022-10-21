b68954a351c8364e18cc419f022f64614e2af02e
Abhijith Rajeev Menon <52545355+RudCodera8@users.noreply.github.com>
2022-10-21 12:17:38 +0530

    Add files via upload

---
+++ smart-contract/truffle-config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // to customize your Truffle configuration!
+
+
+networks: {  
+  	development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*" // Match any network id
+    },
+    privateTestChain: {
+    	host: "127.0.0.1",
+    	port: 8545,
+    	network_id: "15"
+    },
+    compilers: {
+    solc: {
+      version: "^0.8.0"
+    }
+  }
+    
+  }
+
+};
